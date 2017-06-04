8cac7acdfa1bf2129ece2f7e7ebad5380212e223
olegbal <olegbalah@gmail.com>
2017-06-04 19:51:28 +0200

    #1- Rename DregHeapBackendApplication

--- src/main/java/com/github/dregheap/DregHeap.java
+++ src/main/java/com/github/dregheap/DregHeap.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DregHeapBackendApplication {
+public class DregHeap {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DregHeapBackendApplication.class, args);
+		SpringApplication.run(DregHeap.class, args);
 	}
 }
