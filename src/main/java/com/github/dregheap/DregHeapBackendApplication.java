33b97f5a66dfac9f07f0778983e224557e49b3ad
olegbal <olegbalah@gmail.com>
2017-06-04 19:11:55 +0200

    #1 - Create clean spring-boot project

---
+++ src/main/java/com/github/dregheap/DregHeapBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.dregheap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DregHeapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DregHeapBackendApplication.class, args);
+	}
+}
