1d390c87b280cc7b742a53900dc3cd2cace5aa92
olegbal <olegbalah@gmail.com>
2017-06-17 19:19:25 +0300

    Add Post model. Add mongodb connection url to   application.yml config

---
+++ src/main/java/com/github/dregheap/model/Post.java
@@ -0,0 +1,49 @@
+package com.github.dregheap.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "posts")
+public class Post {
+
+    @Id
+    private String id;
+    private String userId;
+    private String topicId;
+    private String postMessage;
+
+    public Post() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getPostMessage() {
+        return postMessage;
+    }
+
+    public void setPostMessage(String postMessage) {
+        this.postMessage = postMessage;
+    }
+}
