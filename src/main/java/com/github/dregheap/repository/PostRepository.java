590631a64c854aaf0e1bdc09b4e73eda86e39945
olegbal <olegbalah@gmail.com>
2017-06-17 19:15:27 +0300

    #14 add PostRepository class

---
+++ src/main/java/com/github/dregheap/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.github.dregheap.repository;
+
+import com.github.dregheap.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, Long> {
+}
