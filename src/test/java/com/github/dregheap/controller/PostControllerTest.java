a0e952380ad85e5c0adffc8e19c4797cd28d4e44
olegbal <olegbalah@gmail.com>
2017-07-05 10:23:00 +0300

    Optimize imports. Remove org.springframework.test.web.servlet.request.MockMvcRequestBuilders.* and add only using imports of this package.

--- src/test/java/com/github/dregheap/controller/PostControllerTest.java
+++ src/test/java/com/github/dregheap/controller/PostControllerTest.java
@@ -1,16 +1,11 @@
 package com.github.dregheap.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dregheap.model.Post;
-import com.github.dregheap.repository.PostRepository;
-import com.github.dregheap.service.DefaultPostService;
 import com.github.dregheap.service.PostService;
-import com.mongodb.util.JSON;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,16 +13,17 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
